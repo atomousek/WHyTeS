b24fbeef77bcff39edf7ddbd1bc02f09d6588a39
martal01 <martin.rektoris@gmail.com>
2019-07-02 17:33:18 +0200

    added expansion

--- src/grid.cpp
+++ src/grid.cpp
@@ -1,1 +1,29 @@
 #include "grid.h"
+
+void expansion(int radius, int dim, std::vector<int> &line, std::vector<vector<int>> &out , int counter ,int max_dim)
+{
+	if(dim > 0)
+	{
+		for(int i = -1*radius; i < radius+1; ++i )
+		{
+			line[dim] = i;
+		}
+		expansion(radius, dim-1, line, out, counter+((2*radius+1)**dim)*(i+radius), max_dim);
+	}
+	else
+	{
+		for(int i = -1*radius; i < radius + 1; ++i)
+		{
+			line[dim] = i;
+			for(int j = 0; j < max_dim; ++j
+			{
+				out[counter][j] = line[j];
+			}
+			++counter;
+		}
+	
+	}
+
+
+}
+
--- src/grid.h
+++ src/grid.h
@@ -2,7 +2,9 @@
 #define __GRID_H__
 
 #include <iostream>
+#include <vector>
 
-void grid(int radius, int dim);
+
+void expansion(int radius, int dim, std::vector<int> &line, std::vector<vector<int>> &out, int counter, int max_dim);
 
 #endif
