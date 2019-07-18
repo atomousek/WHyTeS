3a6f6557915a6846038994fa35317176f20cc338
martal01 <martin.rektoris@gmail.com>
2019-07-18 21:03:17 +0200

    updated

--- src/chronotron_door.py
+++ src/chronotron_door.py
@@ -1,9 +1,10 @@
 import fremen
 import numpy as np
 import directions
+import matplotlib.pyplot as plt
 
 
-path  = '../data/training_data.txt'
+path  = '../data/training_data_short.txt'
 
 longest = 14*24*60*60
 shortest = 60*60
@@ -18,14 +19,12 @@ list_of_periodicities = fremen.build_frequencies(longest, shortest)
 dataset = np.loadtxt(path)
 new_dataset = []
 
-print dataset.shape
 
 for i in range(dataset.shape[0]):
     if dataset[i, 1] != 0:
         new_dataset.append(dataset[i, :])
 
-print dataset.shape
-print len(new_dataset)
+
 
 
 new_dataset = np.array(new_dataset)
@@ -46,16 +45,19 @@ list_of_errors = []
 list_of_fremen_predictions = []
 list_of_fremen_errors = []
 
-T = dataset[:, 0]
-S = dataset[:, -1]
-
 weights = None
+weights_fremen = None
 
 for i in xrange(10):
     print '-----------------------------------------------------------------'
     print 'cycle number ' + str(i)
-    new_dataset = np.loadtxt(path)  # dataset which is not for fremen
-    print dataset.shape
+    new_dataset = np.loadtxt(new_path)  # dataset which is not for fremen
+    dataset = np.loadtxt(path)
+    T = dataset[:, 0]
+    S = dataset[:, -1]
+
+    print 'new_dataset shape: ' +str(new_dataset.shape)
+    print 'dataset shape: ' + str(dataset.shape)
 
 
     P, sum_of_amplitudes = fremen.chosen_period(T, S, list_of_periodicities)
@@ -66,8 +68,7 @@ for i in xrange(10):
 
     #predict for fremen
     #beginning
-    print path
-    fremen_pred = directions.Directions(1, structure, weights)
+    fremen_pred = directions.Directions(1, structure, weights_fremen)
     fremen_pred.fit(path)
     transformed_dataset_fremen, target_fremen = fremen_pred.transform_data(path)
     prediction_fremen = fremen_pred.predict(transformed_dataset_fremen)
@@ -76,9 +77,20 @@ for i in xrange(10):
     error_fremen = fremen_pred.my_rmse(target_fremen, prediction_fremen)    
     list_of_fremen_errors.append(error_fremen)
     weights_fremen = fremen_pred.calc_weights(list_of_fremen_predictions, list_of_fremen_errors)
-    print weights_fremen.shape
+    #print list_of_fremen_predictions[0].shape     
+    #print 'fremen weights shape:' + str(weights_fremen.shape)
     S -= weights_fremen
-    print weights_fremen.shape
+    dataset[:, -1] -= weights_fremen
+    path = 'fremen_dataset.txt'
+    np.savetxt(path, dataset)
+ 
+    
+    plt.plot(prediction_fremen)
+    #plt.scatter(target)
+    plt.ylim(ymax = 1.2, ymin = -0.1)
+    plt.savefig('srovnani_hodnot_uhly_vse'+str(i)+'.png')
+    plt.close()
+
     #end
 
     circle = directions.Directions(1, structure, weights)
@@ -99,9 +111,10 @@ for i in xrange(10):
 
     #weights = np.average(list_of_predictions, weights=list_of_errors)
     weights = circle.calc_weights(list_of_predictions, list_of_errors)
-    dataset[:, -1] -= weights
+    #print weights.shape
+    new_dataset[:, -1] -= weights
     #weights = prediction
    
     new_path = 'dir' + str(i) + '.txt' # new path
-    np.savetxt(new_path, dataset)
+    np.savetxt(new_path, new_dataset)
     print prediction
