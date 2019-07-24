964064291fba9a2af0c983b8b1884c6f60f7a1eb
martal01 <martin.rektoris@gmail.com>
2019-07-24 13:07:44 +0200

    added dataset with minus ones and ones

--- src/adaboost.py
+++ src/adaboost.py
@@ -3,7 +3,7 @@ import directions
 import numpy as np
 
 
-path  = '../data/training_data.txt'
+path  = '../data/new_training_data.txt'
 
 
 longest = 14*24*60*60
@@ -14,8 +14,9 @@ list_of_periodicities = fremen.build_frequencies(longest, shortest)
 dataset = np.loadtxt(path)
 
 
-for i in range(dataset.shape[0]):
-    if dataset[i, 1] == 0:
-        dataset[i, 1] = -1
+#for i in range(dataset.shape[0]):
+#    if dataset[i, 1] == 0:
+#        dataset[i, 1] = -1
 
-np.savetxtx('new_training_data.txt', dataset)
+#dataset.astype(int)
+#np.savetxt('new_training_data.txt', dataset)
