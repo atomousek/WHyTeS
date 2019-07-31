1af4d8495e3be49347df319d72edf15940e8a491
martal01 <martin.rektoris@gmail.com>
2019-07-31 14:30:10 +0200

    clustering

--- src/clustering.py
+++ src/clustering.py
@@ -20,15 +20,45 @@ path = '../data/training_data_ones.txt'
 dataset = np.loadtxt(path)
 
 transformed_data = transform_data(dataset, 60*60*24*7)
+X = transformed_data[:, :2]
 
+"""
 plt.plot(transformed_data[:, 0], transformed_data[:, 1], 'ro')
 plt.ylim(ymax=1.1, ymin=-1.1)
 plt.xlim(xmax=1.1, xmin=-1.1)
 plt.show()
+"""
+db = DBSCAN(eps=0.05 , min_samples= 100).fit(transformed_data[:, :2])
+core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+core_samples_mask[db.core_sample_indices_] = True
+labels =  db.labels_
 
-clustering = DBSCAN(eps=0.05 , min_samples= 100).fit(transformed_data[:, :2])
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+n_noise_ = list(labels).count(-1)
 
-print clustering.labels_
+print 'Estimated number of clusters: %d' % n_clusters_
+print 'Estimated number of noise points: %d' % n_noise_
+
+unique_labels = set(labels)
+colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
+
+for k, col in zip(unique_labels, colors):
+    if k == -1:
+        # Black used for noise.
+        col = [0, 0, 0, 1]
+
+    class_member_mask = (labels == k)
+
+    xy = X[class_member_mask & core_samples_mask]
+    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+             markeredgecolor='k', markersize=14)
+
+    xy = X[class_member_mask & ~core_samples_mask]
+    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+             markeredgecolor='k', markersize=6)
+
+plt.title('Estimated number of clusters: %d' % n_clusters_)
+plt.show()
 
 """
 arr = []
