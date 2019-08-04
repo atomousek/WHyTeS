d6b76f69c063d741a04687ee988a6b7f845280c3
atomousek <atomousek@gmail.com>
2019-08-04 17:23:53 +0200

    integration.pyx out of garbage text

--- src/integration.pyx
+++ src/integration.pyx
@@ -31,7 +31,6 @@ cdef inline double expansion(const double [:] edges, const long [:] no_bins, con
             for id_n_p in xrange(no_periods):
                 bin_centre[base_dim + 2*id_n_p - 1] = cos(time*PI2/periodicities[id_n_p]) 
                 bin_centre[base_dim + 2*id_n_p] = sin(time*PI2/periodicities[id_n_p]) 
-            #for c in xrange(no_clusters)
             for i in xrange(<long>degrees):
                 bin_minus_C[i] = bin_centre[i] - C[i]
             distance = 0.0
@@ -51,7 +50,6 @@ cdef inline double expansion(const double [:] edges, const long [:] no_bins, con
 @cython.wraparound(False)
 def expand(double [:] edges, long [:] no_bins, double [:] starting_points, double [:] periodicities, long dim, double [:] C, double [:,:] PREC, double PI2):
     cdef double sum_of_probs = 0.0
-    #cdef long no_periods = 2*len(periodicities) - 1
     cdef long no_periods = len(periodicities)
     cdef cnp.ndarray[cnp.float64_t, ndim=1] bin_centre = np.zeros(dim + 2*no_periods - 1, dtype=np.float64)
     cdef cnp.ndarray[cnp.float64_t, ndim=1] bin_minus_C = np.zeros(dim + 2*no_periods - 1, dtype=np.float64)
