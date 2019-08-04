6d3cce525bd231a4ff7048eb10d1aa996edfd765
atomousek <atomousek@gmail.com>
2019-08-04 16:04:40 +0200

    frequencies out of commented parts

--- src/frequencies.py
+++ src/frequencies.py
@@ -2,9 +2,6 @@ from sklearn.mixture import GaussianMixture
 import transformation as tr
 import grid
 import full_grid as fg
-#import fremen
-#import transformation_with_dirs as tr
-#import grid_with_directions as grid
 import integration
 import multiprocessing as mp
 
@@ -12,12 +9,8 @@ import multiprocessing as mp
 import matplotlib.pyplot as plt
 import scipy.stats as st
 import numpy as np
-import matplotlib.patches as pat
-import pandas as pd
-import scipy.misc as sm
 
 from time import clock
-#import chi_sqr_gsl
 
 import copy_reg
 import types
@@ -50,10 +43,9 @@ class Frequencies:
 
 
     def _create_model(self, path):
-        C, U, COV, PREC = self._get_params(path)
+        C, U, PREC = self._get_params(path)
         start = clock()
         F = self._calibration_test(path, C, U, PREC)
-        #F = self._calibration(path, C, U, PREC)
         finish = clock()
         print(finish - start)
         return C, F, PREC
@@ -63,20 +55,16 @@ class Frequencies:
         X = tr.create_X(self._get_data(path), self.structure)
         clf = GaussianMixture(n_components=self.clusters, max_iter=500).fit(X)
         labels = clf.predict(X)
-        PREC, COV = self._recalculate_precisions(X, labels)
-        U = clf.predict_proba(X)
+        PREC = self._recalculate_precisions(X, labels)
+        U = np.sum(clf.predict_proba(X), axis=0)
         C = clf.means_
-        return C, U.T, COV, PREC
+        return C, U, PREC
 
 
-    #def _get_data(self, path):
-    #    dataset = np.loadtxt(path)
-    #    return dataset[dataset[:, -1] == 1, : -1]
     def _get_data(self, path):
         """
         """
         dataset = np.loadtxt(path)
-        #all_data = dataset[:, 0: -1]
         training_data = dataset[dataset[:, -1] > 0, 0: -1]
         training_data_values = dataset[dataset[:, -1] > 0, -1]
         # !!! training data values must be whole numbers !!!
@@ -87,99 +75,44 @@ class Frequencies:
                 how_many = training_data_values[i]
                 if how_many > 1.0:
                     copied_data = training_data[i]
-                    #for j in xrange(int(how_many - 1)):
                     for j in range(int(np.ceil(how_many - 1))):
                         new_training.append(copied_data)
-            #print(np.shape(np.array(new_training)))
             training_data = np.r_[training_data, np.array(new_training)]
         return training_data
 
 
     def _recalculate_precisions(self, X, labels):
-        #COV = []
         dim = X.shape[1]
         COV = np.empty((self.clusters, dim, dim))
         for i in xrange(self.clusters):
-            #COV.append(np.cov(X[labels == i].T))
             COV[i] = np.cov(X[labels == i].T)
-        #COV = np.array(COV)
-        #print('X in precision: ' + str(np.shape(X)))
-        #print('COV in precision: ' + str(np.shape(COV)))
-        PREC = np.linalg.inv(COV)
-        return PREC, COV
-
-
-    def _get_density(self, DOMAIN, C, U, P):
-        weights = self._prob_of_belong(DOMAIN, C, P)
-        with np.errstate(divide='raise'):
-            try:
-                density = np.sum(U) / np.sum(weights)
-            except FloatingPointError:
-                print('vahy se souctem 0 nebo nevim')
-                print('np.sum(weights))')
-                print(np.sum(weights))
-                print('np.sum(U[cluster]))')
-                print(np.sum(U))
-                density = 0
-        # F.append(density)
-        return density
-
-    def _calibration(self, path, C, U, PREC):
-        #DOMAIN = tr.create_X(grid.get_domain(np.loadtxt(path), self.edges_of_cell, self.edges_of_cell * 3)[0], self.structure)
-        DOMAIN = tr.create_X(fg.get_full_grid(np.loadtxt(path), self.edges_of_cell)[0], self.structure)
-        #F = []
-
-        #F = np.empty(self.clusters)
-        # for idx in xrange(self.clusters):
-        #     weights = self._prob_of_belong(DOMAIN, C[idx], PREC[idx])
-        #     with np.errstate(divide='raise'):
-        #         try:
-        #             density = np.sum(U[idx]) / np.sum(weights)
-        #         except FloatingPointError:
-        #             print('vahy se souctem 0 nebo nevim')
-        #             print('np.sum(weights))')
-        #             print(np.sum(weights))
-        #             print('np.sum(U[cluster]))')
-        #             print(np.sum(U[idx]))
-        #             density = 0
-        #     #F.append(density)
-        #     F[idx] = density
-
-        #F = np.array(F)
-
-        copy_reg.pickle(types.MethodType, _pickle_method)
-        pool = mp.Pool(min(self.clusters, mp.cpu_count()))
-
-        results = [pool.apply_async(self._get_density, (DOMAIN, C[i], U[i], PREC[i])) for i in xrange(self.clusters)]
-
-        pool.close()
-        pool.join()
-
-        F = [r.get() for r in results]
-        F = np.array(F)
-
-        return F  #, heights
+        return np.linalg.inv(COV)
 
 
     def _get_density_test(self,C, U, P, edges, no_bins, starting_points, periodicities, dim, PI2):
         weights_sum = integration.expand(edges, no_bins, starting_points, periodicities, dim, C, P, PI2)
-        with np.errstate(divide='raise'):
-            try:
-                density = np.sum(U) / weights_sum
-            except FloatingPointError:
-                print('vahy se souctem 0 nebo nevim')
-                print('np.sum(weights))')
-                print weights_sum
-                print('np.sum(U[cluster]))')
-                print(np.sum(U))
-                density = 0
+        if weights_sum == 0:
+            density = 0
+        else:
+            density = U / weights_sum
+        #with np.errstate(divide='raise'):
+        #    try:
+        #        density = U / weights_sum
+        #    except FloatingPointError:
+        #        print('vahy se souctem 0 nebo nevim')
+        #        print('sum of weights')
+        #        print weights_sum
+        #        print('sum of U')
+        #        print(U)
+        #        density = 0
         return density
 
 
     def _calibration_test(self, path, C, U, PREC):
         """
-        fast, somehow functional, but strange otput
+        fast and working - will be used as benchmark
         """
+        # params
         X = np.loadtxt(path)[:, :-1]
         bins_and_ranges = fg.get_bins_and_ranges(X, self.edges_of_cell)
         no_bins = np.array(bins_and_ranges[0])
@@ -188,22 +121,6 @@ class Frequencies:
         periodicities = np.array(self.structure[2])
         dim = np.shape(X)[1]
         PI2 = np.pi*2
-
-        # F = []
-        # for idx in xrange(self.clusters):
-        #     weights_sum = integration.expand(edges, no_bins, starting_points, periodicities, dim, C[idx], PREC[idx], PI2)
-        #     with np.errstate(divide='raise'):
-        #         try:
-        #             density = np.sum(U[idx]) / weights_sum
-        #         except FloatingPointError:
-        #             print('vahy se souctem 0 nebo nevim')
-        #             print('np.sum(weights))')
-        #             print weights_sum
-        #             print('np.sum(U[cluster]))')
-        #             print(np.sum(U[idx]))
-        #             density = 0
-        #     F.append(density)
-        # F = np.array(F)
 
         copy_reg.pickle(types.MethodType, _pickle_method)
         pool = mp.Pool(min(self.clusters, mp.cpu_count()))
@@ -216,7 +133,7 @@ class Frequencies:
         F = [r.get() for r in results]
         F = np.array(F)
 
-        return F  #, heights
+        return F
 
 
     def _get_distrubition(self, idx, X):
@@ -231,11 +148,8 @@ class Frequencies:
             a) input of 1 to n vectors
             b) input of only "raw" vectors (there should be projection of X into hypertime)
         """
-        #DISTR = []
-        #DISTR = np.empty((self.clusters, X.shape[0]))
         copy_reg.pickle(types.MethodType, _pickle_method)
         pool = mp.Pool(min(self.clusters, mp.cpu_count()))
-        #pool = mp.Pool(3)
 
         results = [pool.apply_async(self._get_distrubition, (i, X)) for i in xrange(self.clusters)]
 
@@ -244,14 +158,6 @@ class Frequencies:
 
         DISTR = [r.get() for r in results]
         DISTR = np.array(DISTR)
-
-        #for idx in xrange(self.clusters):
-            #DISTR.append(self.F[idx] * self._prob_of_belong(X, self.C[idx], self.PREC[idx]))
-            #DISTR[idx] = self.F[idx] * self._prob_of_belong(X, self.C[idx], self.PREC[idx])
-        #return np.array(DISTR).max(axis=0)
-        #print('X in _predict_probabilities: ' + str(np.shape(X)))
-        #print('DISTR in _predict_probabilities: ' + str(np.shape(np.array(DISTR))))
-        #return np.array(DISTR).sum(axis=0)
         return DISTR.sum(axis=0)
 
 
@@ -262,10 +168,6 @@ class Frequencies:
         """
         X_C = X - C
         c_dist_x = np.sum(np.dot(X_C, PREC) * X_C, axis=1)
-        #c_dist_x = []
-        #for x_c in X_C:
-        #    c_dist_x.append(np.dot(np.dot(x_c.T, PREC), x_c))
-        #c_dist_x = np.array(c_dist_x)
         return 1 - st.chi2.cdf(c_dist_x, len(C))
 
 
@@ -297,7 +199,6 @@ class Frequencies:
     def poisson(self, path):
         X, K = self._transform_data_over_grid(path)
         Lambda = self._predict_probabilities(X)
-        #probs = st.poisson.cdf(K, Lambda)
         probs = st.poisson.cdf(K, Lambda)
         probs[(probs>0.94) & (K==0)] = 0.5
         ################## only for one-time visualisation #########
