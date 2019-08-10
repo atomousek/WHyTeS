dd1107c2609a746de6994ccc82211b7ea7838b3e
keremeyisoy <kerem.eyisoy@gmail.com>
2019-08-10 15:46:48 +0200

    fremen variable type changed and code commented

--- src/fremen_wrapper.py
+++ src/fremen_wrapper.py
@@ -1,7 +1,12 @@
-import os
 import subprocess
 
 def get_periodicities(path, number_of_periods=5, max_periods=60*60*24*7):
+    """
+    :param path: Path for the training data with 2 columns in the following order;   "timestamp occurrence(1/0)"
+    :param number_of_periods:   number of periods you want in the return array
+    :param max_period: maximum period
+    :return: array of most fundamental periodicities
+    """
     cmd = "./fremen " + str(path) + " " + str(number_of_periods) + " " + str(max_periods)
     output = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout
     output = output.read()
