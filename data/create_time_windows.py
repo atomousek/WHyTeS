d0c2de96737d785632af120ffd749ac26ee1e7c4
atomousek <atomousek@gmail.com>
2019-08-30 03:43:55 +0200

    removed

--- data/create_time_windows.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-data = np.loadtxt('test_dataset_all.txt')
-list_of_times = np.loadtxt('time_windows/testing_times.txt')
-
-for time in list_of_times:
-    part = data[(data[:,0] > time - 10.0) & (data[:,0] < time + 50.0), :]
-    np.savetxt('time_windows/' + str(int(time)) + '_test_data.txt', part)
